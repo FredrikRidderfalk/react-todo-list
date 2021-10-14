37e290377cb232d6ef958ae64640c647a04a1dcd
Fredrik Ridderfalk <ridderfalk@gmail.com>
2021-10-14 13:25:26 +0200

    updated App with state

--- src/App.js
+++ src/App.js
@@ -23,12 +23,22 @@ class App extends React.Component {
   //   console.log("hello habibi");
   // }
 
+  // then there's this thing called constructor method. Props is immutable, meaning it's a way to pass information between components, but with props the information can't be changed by the receiving component. That's when we use State. State needs the constructor method. State only works with class-based components. Constructor is a specific method built into JavaScript, that initializes parts of this class that it's in. We also need super(). What super() does is it goes to the parent class (also called super class), and brings information from it so that our component can use those goodies.
+
+  constructor() {
+    super();
+    this.state = {
+      answer: "Yes",
+    };
+  }
+
   render() {
     // the method would then be called here, inside the render function, before the return function.
     // const style = this.myMethodHere();
     return (
       <div>
         <Header />
+        <h1>Is state important to know? {this.state.answer}</h1>
         <MainContent />
         <Footer />
       </div>
