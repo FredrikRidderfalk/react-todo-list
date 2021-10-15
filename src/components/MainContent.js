f3565b3ef67c61f1b39c1993d8e90642b57fa865
Fredrik Ridderfalk <ridderfalk@gmail.com>
2021-10-15 12:03:55 +0200

    added styling to completed items

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -2,12 +2,47 @@ import React from "react";
 import TodoItem from "./TodoItem";
 import todosData from "./todosData";
 
-function MainContent() {
-  const todoItems = todosData.map((item) => (
-    <TodoItem key={item.id} item={item} />
-  ));
+// function MainContent() {
+//   const todoItems = todosData.map((item) => (
+//     <TodoItem key={item.id} item={item} />
+//   ));
 
-  return <main className="todo-list">{todoItems}</main>;
+//   return <main className="todo-list">{todoItems}</main>;
+// }
+
+class MainContent extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todosData,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(id) {
+    this.setState((prevState) => {
+      const updatedTodos = prevState.todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      });
+      return {
+        todos: updatedTodos,
+      };
+    });
+  }
+
+  render() {
+    const todoItems = this.state.todos.map((item) => (
+      <TodoItem key={item.id} item={item} handleChange={this.handleChange} />
+    ));
+
+    return <main className="todo-list">{todoItems}</main>;
+  }
 }
 
 export default MainContent;
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,29 @@
 import React from "react";
 
 function TodoItem(props) {
+  const completedItemStyle = () => {
+    const styling = {
+      color: "gray",
+      textDecoration: "line-through",
+    };
+    if (props.item.completed) {
+      return styling;
+    }
+  };
+
   return (
     <div className="todo-item">
       <input
         type="checkbox"
         checked={props.item.completed}
+        onChange={() => props.handleChange(props.item.id)}
         className="checkbox"
-        id="panties"
-        name="panties"
+        id={props.item.id}
+        name={props.item.id}
       />
-      <label for="panties">{props.item.text}</label>
+      <label for={props.item.id} style={completedItemStyle()}>
+        {props.item.text}
+      </label>
     </div>
   );
 }
